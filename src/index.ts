e0d4c506658f81e761e2d569c6544826916ad6c5
ApOszTroF <tasztalos69@gmail.com>
2021-03-28 14:37:58 +0200

    read key file with fs & app logic

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,101 @@
 import { Command, flags } from '@oclif/command';
+import * as admin from 'firebase-admin';
+import * as ora from 'ora';
+import * as chalk from 'chalk';
+import { readFile } from 'fs';
+import { promisify } from 'util';
+
+const err = (text: string) => {
+  console.log(chalk.red(text));
+  process.exit(1);
+};
+
+const readFileAwait = promisify(readFile);
+
+const readJson = async (path: string) => {
+  const file = await readFileAwait(require.resolve(path));
+  return JSON.parse(file.toString());
+};
 
 class LifetrackerAdmin extends Command {
   static description = 'describe the command here';
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    serviceAccount: flags.string({ char: 's' }),
+    observer: flags.boolean({ char: 'o' }),
+    contentManager: flags.boolean({ char: 'c' }),
+    revoke: flags.boolean({ char: 'r' }),
   };
 
-  static args = [{ name: 'userID' }];
+  static args = [{ name: 'userUID', required: true }];
 
   async run() {
     const { args, flags } = this.parse(LifetrackerAdmin);
 
-    const name = flags.name ?? 'world';
-    this.log(`hello ${name} from ./src/index.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    const { revoke, observer, contentManager } = flags;
+
+    if ((revoke && observer) || (revoke && contentManager))
+      err(
+        'You cannot provide and revoke roles simultaneously. Either use revoke or role commands.',
+      );
+
+    try {
+      const serviceAccount = await readJson(
+        flags.serviceAccount || './fbadmin.private.json',
+      );
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    } catch (error) {
+      if (error.code === 'MODULE_NOT_FOUND')
+        err(
+          `No service account file found. Please provide one in the current directory named ${chalk.bold(
+            'fbadmin.private.json.',
+          )}`,
+        );
+    }
+
+    const resolveMode = () => {
+      interface Roles {
+        observer: boolean;
+        contentManager: boolean;
+      }
+      const defaultRoles = {
+        observer: false,
+        contentManager: false,
+      };
+
+      let returnRoles: Roles = defaultRoles;
+
+      if (observer) returnRoles.observer = true;
+
+      if (contentManager) {
+        returnRoles.contentManager = true;
+        returnRoles.observer = true;
+      }
+
+      if (revoke) returnRoles = defaultRoles;
+      return returnRoles;
+    };
+
+    const roles = resolveMode();
+
+    const spinnerText = revoke
+      ? 'Revoking roles...'
+      : `Setting roles ${observer || contentManager ? 'observer' : ''} ${
+          contentManager ? '& content manager' : ''
+        }...`;
+
+    const spinner = ora(chalk.cyan(spinnerText)).start();
+    try {
+      await admin.auth().setCustomUserClaims(args.userUID, roles);
+      spinner.succeed(chalk.green(spinnerText + ' Done!'));
+      process.exit(0);
+    } catch (error) {
+      spinner.fail();
+      err(error.errorInfo.message);
     }
   }
 }
